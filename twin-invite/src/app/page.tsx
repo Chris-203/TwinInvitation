ee9cd772fb2df54fda03503a66d9dd06dfa71936
Christopher Cortazo <cortazo.christopher@gmail.com>
2025-03-20 18:39:00 -0400

    test2.0

--- twin-invite/src/app/page.tsx
+++ twin-invite/src/app/page.tsx
@@ -1,9 +1,28 @@
 "use client";
 
-import { Box, Typography } from "@mui/material";
-import React from "react";
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import React, {useState, useEffect} from "react";
 
 export default function Invite() {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const [viewBoxHeight, setViewBoxHeight] = useState(105);
+
+  useEffect(() => {
+    const updateViewBox = () => {
+      if (window.innerWidth < 600) {
+        setViewBoxHeight(75); // Small screens
+      } else {
+        setViewBoxHeight(105); // Large screens
+      }
+    };
+  
+    updateViewBox(); // Run on initial load
+    window.addEventListener("resize", updateViewBox);
+  
+    return () => window.removeEventListener("resize", updateViewBox);
+  }, []);
+
   return (
     <Box
       sx={{
@@ -13,19 +32,10 @@ export default function Invite() {
         justifyContent: "center",
         alignItems: "center",
         overflow: "hidden",
-
-        "@media (orientation: landscape)": {
-          width: "100vh", // Swap width and height in landscape mode
-          height: "100vw",
-          transform: "none", // Rotate the content
-          transformOrigin: "center",
-        },
       }}
     >
       <Box
         sx={{
-          alignContent: "center",
-          justifyContent: "center",
           display: "flex",
           flexDirection: "column",
           maxWidth: "100%",
@@ -42,129 +52,167 @@ export default function Invite() {
           backgroundPosition: "center",
         }}
       >
-        <Box sx={{ marginTop: 3 }}>
-          <Box sx={{}}>
+        <Box
+          sx={{
+            marginTop: 3,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
             <svg
               width="100%"
-              style={{
-                position: "relative",
-                left: 0,
-              }}
+              viewBox={`0 0 400 ${viewBoxHeight}`}
+              style={{ backgroundColor: "teal" }}
             >
               <defs>
                 <path
-                  id="curve"
+                  id="curve1"
                   d="M 50,150 A 150,100 0 0,1 350,150"
                   fill="transparent"
                 />
               </defs>
               <text
                 fill="white"
-                fontSize="30"
+                fontSize={isSmallScreen ? "20" : "30"}
                 fontWeight="bold"
                 fontFamily="Fresh Palm"
               >
-                <textPath href="#curve" startOffset="50%" textAnchor="middle">
+                <textPath href="#curve1" startOffset="50%" textAnchor="middle">
                   Te invitamos a celebrar nuestro
                 </textPath>
               </text>
             </svg>
           </Box>
-          <svg
-            width="100%"
-            style={{ position: "relative", top: -100, left: 0 }}
+
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              // marginTop: -6,
+            }}
           >
-            <defs>
-              <path
-                id="curve"
-                d="M 50,150 A 150,100 0 0,1 350,150"
-                fill="transparent"
-              />
-            </defs>
-            <text fill="white" fontSize="32" fontWeight="bold">
-              <textPath
-                href="#curve"
-                startOffset="50%"
-                textAnchor="middle"
-                fontFamily="Fresh Palm"
+            <svg
+              width="100%"
+              viewBox="0 25 400 50"
+              style={{ backgroundColor: "maroon" }}
+            >
+              <defs>
+                <path
+                  id="curve2"
+                  d="M 50,150 A 150,100 0 0,1 350,150"
+                  fill="transparent"
+                />
+              </defs>
+              <text
+                fill="white"
+                fontSize={isSmallScreen ? "24" : "32"}
+                fontWeight="bold"
               >
-                CUMPLEAÑOS
-              </textPath>
-            </text>
-          </svg>
+                <textPath
+                  href="#curve2"
+                  startOffset="50%"
+                  textAnchor="middle"
+                  fontFamily="Fresh Palm"
+                >
+                  CUMPLEAÑOS
+                </textPath>
+              </text>
+            </svg>
+          </Box>
+
           <Typography
             variant="h4"
             align="center"
             sx={{
               fontWeight: "bold",
               fontFamily: "Fresh Palm",
-              fontSize: 100,
-              width: "20%",
-              margin: "auto",
-              marginTop: -25,
+              fontSize: isSmallScreen ? 60 : 100,
+              // width: "100%",
+              marginTop: -2,
               color: "white",
+              backgroundColor: "teal",
             }}
           >
             15
           </Typography>
+
           <Typography
             variant="h4"
             align="center"
             sx={{
               fontFamily: "Shine in Valentine",
-              fontSize: 90,
+              fontSize: isSmallScreen ? 50 : 90,
+              color: "white",
+              backgroundColor: "maroon",
             }}
           >
             Joseph
           </Typography>
+
           <Typography
             variant="h4"
             align="center"
             sx={{
               fontFamily: "Shine in Valentine",
-              fontSize: 40,
+              fontSize: isSmallScreen ? 30 : 40,
               height: "6%",
-              marginBottom: -1,
+              marginBottom: -2,
+              color: "white",
+              backgroundColor: "maroon",
             }}
           >
             Y
           </Typography>
+
           <Typography
             variant="h4"
             align="center"
             sx={{
               fontFamily: "Shine in Valentine",
-              fontSize: 90,
+              fontSize: isSmallScreen ? 50 : 90,
+              color: "white",
+              backgroundColor: "maroon",
             }}
           >
             Jovanni
           </Typography>
 
-          <svg
-            width="100%"
-            style={{
-              position: "relative",
-              left: 0,
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              // width: "100%",
+              backgroundColor: "maroon",
+              // marginTop: -3,
             }}
           >
-            <defs>
-              <path
-                id="curve"
-                d="M 50,150 A 150,100 0 0,1 350,150"
-                fill="transparent"
-              />
-            </defs>
-            <text fill="white" fontSize="32" fontWeight="bold">
-              <textPath
-                href="#curve"
-                startOffset="50%"
-                textAnchor="middle"
-                fontFamily="Fresh Palm"
+            <svg width="75%" viewBox="0 0 400 200">
+              <defs>
+                <path
+                  id="curve3"
+                  d="M 50,150 A 150,100 0 0,1 350,150"
+                  fill="transparent"
+                />
+              </defs>
+              <text
+                fill="white"
+                fontSize={isSmallScreen ? "32" : "32"}
+                fontWeight="bold"
               >
-                ABRIL
-              </textPath>
-            </text>
-          </svg>
+                <textPath
+                  href="#curve3"
+                  startOffset="50%"
+                  textAnchor="middle"
+                  fontFamily="Fresh Palm"
+                >
+                  ABRIL
+                </textPath>
+              </text>
+            </svg>
+          </Box>
+
           <Typography
             variant="h4"
             align="center"
@@ -182,57 +230,55 @@ export default function Invite() {
               sx={{
                 borderTop: "2px solid black",
                 borderBottom: "2px solid black",
+                color: "white",
               }}
             >
-              {" "}
-              SABADO{" "}
-            </Box>{" "}
+              SABADO
+            </Box>
             <Box
               sx={{
-                width: 55, // Adjust size of circle
-                height: 55,
-                fontSize: 60,
+                width: isSmallScreen ? 40 : 55,
+                height: isSmallScreen ? 40 : 55,
+                fontSize: isSmallScreen ? 40 : 60,
                 fontWeight: "bold",
                 lineHeight: ".8",
+                color: "white",
               }}
             >
               5
-            </Box>{" "}
+            </Box>
             <Box
               sx={{
                 borderTop: "2px solid black",
                 borderBottom: "2px solid black",
+                color: "white",
               }}
             >
-              {" "}
-              5:00PM{" "}
-            </Box>{" "}
+              5:00PM
+            </Box>
           </Typography>
-          <Box
+
+          <Typography
+            variant="h4"
+            align="center"
             sx={{
-              display: "flex",
-              flexDirection: "column",
-              gap: 0,
+              fontWeight: "bold",
+              fontFamily: "Fresh Palm",
+              color: "white",
             }}
           >
-            <Typography
-              variant="h4"
-              align="center"
-              sx={{
-                fontWeight: "bold",
-                fontFamily: "Fresh Palm",
-              }}
-            >
-              29 POPLAR STREET
-            </Typography>
-          </Box>
+            29 POPLAR STREET
+          </Typography>
+
           <Typography
             variant="h6"
             align="center"
             sx={{
               fontFamily: "Shine in Valentine",
-              marginTop: 8,
+              marginTop: isSmallScreen ? 4 : 8,
               marginBottom: 15,
+              fontSize: isSmallScreen ? 25 : 30,
+              color: "white",
             }}
           >
             lluvia de sobres
